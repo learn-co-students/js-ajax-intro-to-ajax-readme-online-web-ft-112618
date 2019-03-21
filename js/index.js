fe0cfeaef8d3467446fdac324a22f1f8c52f789a
Andrew Johnson <ajohnson.uva@gmail.com>
2019-03-21 17:20:19 -0400

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
 // your code here
+function showCommits() {
+  const commits = JSON.parse(this.responseText)
+  const commitsList = `<ul>${commits.map(c => `<li><strong>${c.author.login}</strong> - ${c.commit.message}</li>`).join('')}</ul>`
+  document.getElementById('commits').innerHTML = commitsList
+}
+
+function getCommits(el) {
+  const name = el.dataset.repo;
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', showCommits);
+  req.open('GET', 'https://api.github.com/repos/octocat/' + name + '/commits');
+  req.send();
+}
+
+function showRepositories() {
+  var repos = JSON.parse(this.responseText);
+  const repoList = `<ul>${repos
+    .map(r => `<li>${r.name} - <a href="#" data-repo="${r.name}" onclick="getCommits(this)">Get Commits</a></li>`)
+    .join('')}</ul>`;
+  document.getElementById('repositories').innerHTML = repoList;
+}
+
+function getRepositories() {
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', showRepositories);
+  req.open('GET', 'https://api.github.com/users/octocat/repos');
+  req.send();
+}
